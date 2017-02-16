32af46567bd5c870b1e3744005cc790f65a5b062
N0nki <rgstr_land@icloud.com>
2017-02-17 03:08:57 +0900

    first commit

---
+++ nvim/setup_nvim.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# neovim
+cd ~/.config
+mkdir nvim
+cd nvim
+mkdir colors
+# install dein.vim
+cd ~/.cache
+mkdir dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+ln -sf ~/dotfiles/nvim/.dein.toml ~/.dein.toml
+ln -sf ~/dotfiles/nvim/.dein_lazy.toml ~/.dein_lazy.toml
+ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/nvim/colors/gruvbox.vim ~/.config/nvim/colors/gruvbox.vim
+ln -sf ~/dotfiles/nvim/colors/neodark.vim ~/.config/nvim/colors/neodark.vim
+ln -sf ~/dotfiles/nvim/colors/onedark.vim ~/.config/nvim/colors/onedark.vim
+ln -sf ~/dotfiles/nvim/colors/tender.vim ~/.config/nvim/colors/tender.vim
+
