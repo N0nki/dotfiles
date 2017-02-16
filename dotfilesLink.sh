20e69c20206603649aa2c13cb855129511bd582e
N0nki <rgstr_land@icloud.com>
2017-02-17 05:06:55 +0900

    add macos and vim setup scripts

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 cd ~/
+# macOS
+sh ~/dotfiles/macOS/setup_macos.sh
+
 # vim
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/colors ~/.vim
+sh ~/dotfiles/vim/setup_vim.sh
 
 # atom
 sh ~/dotfiles/.atom/setup_atom.sh
