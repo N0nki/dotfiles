83015ec81a28843ceb881bcd816034929104d024
N0nki <rgstr_land@icloud.com>
2017-02-17 15:45:16 +0900

    add .xvimrc

--- macOS/setup_macos.sh
+++ macOS/setup_macos.sh
@@ -3,3 +3,4 @@
 ln -sf ~/dotfiles/macOS/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/macOS/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/macOS/.latexmkrc ~/.latexmkrc
+ln -sf ~/dotfiles/macOS/.xvimrc ~/.xvimrc
