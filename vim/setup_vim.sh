52ba00f5edc7885e60ec5df4dcdba6804071926c
N0nki <rgstr_land@icloud.com>
2017-04-20 17:04:40 +0900

    disable NeoBundle setup script

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -1,6 +1,9 @@
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
-sh ./install.sh
-rm ./install.sh
+# curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+# sh ./install.sh
+# rm ./install.sh
+
+# プラグインマネージャーはdeinへ移行
+# deinのインストールはneovimのセットアップスクリプトで行うので、ここでは不要
 
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/.gvimrc ~/.gvimrc
