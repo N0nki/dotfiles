317801418caeb3d0ecdcedb531c6d1d940d0b628
N0nki <rgstr_land@icloud.com>
2017-04-27 22:16:42 +0900

    add synbolic links

--- nvim/synbolic_links.sh
+++ nvim/synbolic_links.sh
@@ -1,10 +1,20 @@
+# neovim and dein 
+ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/nvim/keymap.rc.vim ~/.config/nvim/keymap.rc.vim
+ln -sf ~/dotfiles/nvim/options.rc.vim ~/.config/nvim/options.rc.vim
 ln -sf ~/dotfiles/nvim/.dein.toml ~/.dein.toml
 ln -sf ~/dotfiles/nvim/.dein_lazy.toml ~/.dein_lazy.toml
-ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+# colorscheme
 ln -sf ~/dotfiles/nvim/colors/gruvbox.vim ~/.config/nvim/colors/gruvbox.vim
 ln -sf ~/dotfiles/nvim/colors/neodark.vim ~/.config/nvim/colors/neodark.vim
 ln -sf ~/dotfiles/nvim/colors/onedark.vim ~/.config/nvim/colors/onedark.vim
 ln -sf ~/dotfiles/nvim/colors/tender.vim ~/.config/nvim/colors/tender.vim
 ln -sf ~/dotfiles/nvim/colors/iceberg.vim ~/.config/nvim/colors/iceberg.vim
+# plugin files
 ln -sf ~/dotfiles/nvim/plugins/vimfiler.rc.vim ~/.config/nvim/plugins/vimfiler.rc.vim
 ln -sf ~/dotfiles/nvim/plugins/unite.rc.vim ~/.config/nvim/plugins/unite.rc.vim
+ln -sf ~/dotfiles/nvim/plugins/vimshell.rc.vim ~/.config/nvim/plugins/vimshell.rc.vim
+ln -sf ~/dotfiles/nvim/plugins/deoplete.rc.vim ~/.config/nvim/plugins/deoplete.rc.vim
+ln -sf ~/dotfiles/nvim/plugins/airline.rc.vim ~/.config/nvim/plugins/airline.rc.vim
+ln -sf ~/dotfiles/nvim/plugins/indentline.rc.vim ~/.config/nvim/plugins/indentline.rc.vim
+ln -sf ~/dotfiles/nvim/plugins/vimtex.rc.vim ~/.config/nvim/plugins/vimtex.rc.vim
