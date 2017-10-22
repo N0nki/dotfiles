e501fd6d25194bd5a0211df5fb9c54ef2694ac32
N0nki <rgstr_land@icloud.com>
2017-10-22 23:03:05 +0900

    update

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -13,7 +13,28 @@ defaults write com.apple.dock autohide -bool true
 defaults write -g AppleShowAllExtensions -bool true
 # 隠しファイルを表示
 defaults write com.apple.finder AppleShowAllFiles -bool true
+# デフォルトでホームフォルダを開く
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+# ステータスバーを表示
+defaults write com.apple.finder ShowStatusBar -bool true
+# パスバーを表示
+defaults write com.apple.finder ShowPathbar -bool true
+# タブバーを表示
+defaults write com.apple.finder ShowTabView -bool true
+# ライブラリを表示
+chflags nohidden ~/Library
+
+### Terminal
+# TerminalのテーマをIcebergに変更
+open "$HOME/dotfiles/macOS/termcolor/terminalapp/Iceberg.terminal"
+sleep 1 # Wait a bit to make sure the theme is loaded
+defaults write com.apple.terminal "Default Window Settings" -string "Iceberg"
+defaults write com.apple.terminal "Startup Window Settings" -string "Iceberg"
 
 ### Other
 # バッテリー残量の%表示
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+killall Finder
+killall Dock
