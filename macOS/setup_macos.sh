20d310f463e2b8094895a8448b8ceac80f207778
N0nki <rgstr_land@icloud.com>
2017-11-15 22:14:52 +0900

    update

--- macOS/setup_macos.sh
+++ macOS/setup_macos.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+curl -L -o ~/Downloads/iTerm2-3_1_5.zip https://iterm2.com/downloads/stable/iTerm2-3_1_5.zip
 ln -sf ~/dotfiles/macOS/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/macOS/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/macOS/.latexmkrc ~/.latexmkrc
