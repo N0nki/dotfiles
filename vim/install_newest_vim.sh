ed9bef98ac878097c774a8266ab37c31331c87de
N0nki <rgstr_land@icloud.com>
2017-11-21 14:25:40 +0900

    update configure

--- vim/install_newest_vim.sh
+++ vim/install_newest_vim.sh
@@ -5,7 +5,7 @@ cd ~/vim
 git pull
 cd ~/vim/src/
 ./configure --with-features=huge --enable-gui=gnome2 --enable-perlinterp --enable-pythoninterp --enable-rubyinterp --enable-fail-if-missing --prefix=$HOME/usr
-# ./configure --with-features=huge --enable-gui=gnome2 --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp --enable-luainterp --with-luajit --enable-fail-if-missing
+# ./configure --with-features=huge --enable-gui=gnome2 --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp --enable-luainterp --with-luajit --with-lua-prefix=/usr/local --enable-fail-if-missing --prefix=$HOME/usr
 make
 make install
 # echo 'alias vim="~/usr/bin/vim"' >> ~/.bashrc
