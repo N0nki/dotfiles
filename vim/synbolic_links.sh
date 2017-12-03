10ff97e7cc6fb35a22f2f79867f5e2d53ca0668d
N0nki <rgstr_land@icloud.com>
2017-12-03 23:15:18 +0900

    update

--- vim/synbolic_links.sh
+++ vim/synbolic_links.sh
@@ -11,4 +11,13 @@ ln -sf ~/dotfiles/vim/options.rc.vim ~/.config/nvim/options.rc.vim
 ln -sf ~/dotfiles/vim/.dein.toml ~/.dein.toml
 ln -sf ~/dotfiles/vim/.dein_lazy.toml ~/.dein_lazy.toml
 # plugin files
-ln -sf ~/dotfiles/vim/plugins ~/.config/nvim/plugins
+ln -sf ~/dotfiles/vim/plugins/vimfiler.rc.vim ~/.config/nvim/plugins/vimfiler.rc.vim
+ln -sf ~/dotfiles/vim/plugins/unite.rc.vim ~/.config/nvim/plugins/unite.rc.vim
+ln -sf ~/dotfiles/vim/plugins/vimshell.rc.vim ~/.config/nvim/plugins/vimshell.rc.vim
+ln -sf ~/dotfiles/vim/plugins/deoplete.rc.vim ~/.config/nvim/plugins/deoplete.rc.vim
+ln -sf ~/dotfiles/vim/plugins/airline.rc.vim ~/.config/nvim/plugins/airline.rc.vim
+ln -sf ~/dotfiles/vim/plugins/indentline.rc.vim ~/.config/nvim/plugins/indentline.rc.vim
+ln -sf ~/dotfiles/vim/plugins/vimtex.rc.vim ~/.config/nvim/plugins/vimtex.rc.vim
+ln -sf ~/dotfiles/vim/plugins/quickrun.rc.vim ~/.config/nvim/plugins/quickrun.rc.vim
+ln -sf ~/dotfiles/vim/plugins/vim-markdown.rc.vim ~/.config/nvim/plugins/vim-markdown.rc.vim
+ln -sf ~/dotfiles/vim/plugins/neosnippet.rc.vim ~/.config/nvim/plugins/neosnippet.rc.vim
