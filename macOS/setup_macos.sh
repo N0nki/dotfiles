e7f152fac2f940ec48da7fd3ec422aeb7a167f40
N0nki <rgstr_land@icloud.com>
2021-09-02 01:50:53 +0900

    move pylintrc to python/

--- macOS/setup_macos.sh
+++ macOS/setup_macos.sh
@@ -6,5 +6,4 @@ ln -sf ~/dotfiles/macOS/.latexmkrc ~/.latexmkrc
 ln -sf ~/dotfiles/macOS/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/macOS/.vrapperrc ~/.vrapperrc
 ln -sf ~/dotfiles/macOS/.xvimrc ~/.xvimrc
-ln -sf ~/dotfiles/macOS/pylintrc ~/.config/pylintrc
 sh ./defaults.sh
---
+++ python/symbolic_link.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/python/pylintrc ~/.config/pylintrc
+
