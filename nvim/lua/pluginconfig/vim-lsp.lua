2abd074c1a22ee9d9fdefc93a2b3b30558b5a65b
N0nki <rgstr_land@icloud.com>
2022-10-11 00:16:48 +0900

    update vim-lsp settings

--- nvim/lua/pluginconfig/vim-lsp.lua
+++ nvim/lua/pluginconfig/vim-lsp.lua
@@ -1,7 +1,11 @@
 vim.g.lsp_diagnostics_enabled = 1
-vim.g.lsp_virtual_text_enabled = 1
+vim.g.lsp_diagnostics_virtual_text_enabled = 1
 vim.g.lsp_diagnostics_echo_cursor = 1
-vim.g.lsp_highlights_enabled = 0
+vim.g.lsp_diagnostics_highlights_enabled = 0
+vim.g.lsp_diagnostics_virtual_text_prefix = " ‣ "
+vim.g.lsp_diagnostics_signs_error = {text =  "❌"}
+vim.g.lsp_diagnostics_signs_warning = {text =  "⚠️"}
+vim.g.lsp_diagnostics_signs_hint = {text =  "☝️"}
 
 local pyls_workspace_config = {
   pyls = {
