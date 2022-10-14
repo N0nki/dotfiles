4a51fcd3f3ee18c03a329d370821516b6ef06464
N0nki <rgstr_land@icloud.com>
2022-10-15 03:39:36 +0900

    remove plugin/

--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -9,7 +9,6 @@ mkdir nvim
 
 ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 ln -sf ~/dotfiles/nvim/lua ~/.config/nvim/lua
-ln -sf ~/dotfiles/nvim/plugin ~/.config/nvim/plugin
 ln -sf ~/dotfiles/nvim/ftdetect ~/.config/nvim/ftdetect
 ln -sf ~/dotfiles/nvim/ftplugin ~/.config/nvim/ftplugin
 ln -sf ~/dotfiles/nvim/snippets ~/.config/nvim/snippets
