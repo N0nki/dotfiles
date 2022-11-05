2ac549a9e53bf3c5f69ebc2b237fc4ea05ea031d
n0nki <rgstr_land@icloud.com>
2022-11-06 02:23:48 +0900

    avoid keybind duplication: Windows Ctrl-v, tmux prefix Ctrl-t

--- nvim/lua/pluginconfig/fzf-vim.lua
+++ nvim/lua/pluginconfig/fzf-vim.lua
@@ -4,6 +4,13 @@ local opts = {noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 local command = vim.api.nvim_create_user_command
 
+-- avoid keybind duplication: Windows Ctrl-v, tmux prefix Ctrl-t
+vim.g.fzf_action = {
+  ["ctrl-r"] = "tab split",
+  ["ctrl-s"] = "split",
+  ["ctrl-f"] = "vsplit",
+}
+
 keymap("n", "<Leader>df", ":<C-u>Files<CR>", opts)
 keymap("n", "<Leader>dl", ":<C-u>Lines<CR>", opts)
 keymap("n", "<Leader>da", ":<C-u>Ag<CR>", opts)
