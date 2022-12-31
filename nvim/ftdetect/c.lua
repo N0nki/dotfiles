a01c47d1a124ca61041e6ca7bb2e2ad6aa25080c
N0nki <rgstr_land@icloud.com>
2023-01-01 01:36:12 +0900

    remove ftdetect

--- nvim/ftdetect/c.lua
+++
@@ -1,6 +0,0 @@
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd({"BufNewFile, BufRead"}, {
-        pattern = {"*.c", "*.h"},
-        command = "setlocal tabstop=4 softtabstop=4 shiftwidth=4",
-})
