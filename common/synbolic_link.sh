b7f8dbe8bceb09c1965cc9c85d4e6b3e9cecb2e0
n0nki <rgstr_land@icloud.com>
2023-01-16 01:34:29 +0900

    add tmux plugin

--- common/synbolic_link.sh
+++ common/synbolic_link.sh
@@ -4,4 +4,5 @@
 ln -sf ~/dotfiles/common/starship.toml ~/.config/starship.toml
 
 # tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf ~/dotfiles/common/.tmux.conf ~/.tmux.conf
