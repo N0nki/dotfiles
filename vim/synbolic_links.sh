f9fa12c37d2600cb83404303e7ef7f34627663ee
N0nki <rgstr_land@icloud.com>
2023-01-22 22:09:08 +0900

    fix plugin config path

--- vim/synbolic_links.sh
+++ vim/synbolic_links.sh
@@ -7,6 +7,6 @@ ln -sf ~/dotfiles/vim/options.rc.vim ~/.vim
 ln -sf ~/dotfiles/vim/.dein.toml ~/.dein.toml
 ln -sf ~/dotfiles/vim/.dein_lazy.toml ~/.dein_lazy.toml
 # plugin files
-ln -sf ~/dotfiles/vim/plugins ~/.config/nvim/plugins
+ln -sf ~/dotfiles/vim/plugins ~/.vim/plugins
 # snippet
 ln -sf ~/dotfiles/vim/snippets ~/.vim/snippets
