db4a160468cb01c05544f7bdcacbedf2af50c823
N0nki <rgstr_land@icloud.com>
2025-06-30 00:48:42 +0900

    update mason-lspconfig

--- nvim/lua/pluginconfig/mason-lspconfig.lua
+++ nvim/lua/pluginconfig/mason-lspconfig.lua
@@ -20,7 +20,7 @@ end
 
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-require("mason-lspconfig").setup_handlers({
+require("mason-lspconfig").setup({
   function(server_name)
     lspconfig[server_name].setup({
       capabilities = capabilities,
