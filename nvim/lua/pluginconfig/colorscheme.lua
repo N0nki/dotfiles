dfcac5866ea6da37f99a4234e76544fa8acd91ca
N0nki <rgstr_land@icloud.com>
2025-11-29 03:52:03 +0900

    fmt

--- nvim/lua/pluginconfig/colorscheme.lua
+++ nvim/lua/pluginconfig/colorscheme.lua
@@ -1,7 +1,8 @@
 -- colorscheme
 
-vim.cmd([[colorscheme iceberg]])
---  vim.cmd([[colorscheme icy]])
+vim.cmd([[colorscheme vague]])
+-- vim.cmd([[colorscheme iceberg]])
+-- vim.cmd([[colorscheme icy]])
 -- vim.cmd([[colorscheme kanagawa]])
 -- vim.cmd([[colorscheme tokyonight]])
 -- vim.cmd([[colorscheme nightfox]])
