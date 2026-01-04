7df04f036dbbf62862d7406f2c2fc3bbdcf1d616
N0nki <rgstr_land@icloud.com>
2026-01-05 01:24:26 +0900

    update setup_macos.sh

--- platforms/macOS/setup_macos.sh
+++ platforms/macOS/setup_macos.sh
@@ -11,6 +11,8 @@ if [ ! -d "/Applications/Xcode.app" ]; then
   exit 1
 fi
 
+git clone --recurse-submodules https://github.com/N0nki/dotfiles.git ~/dotfiles
+
 ln -sf ~/dotfiles/platforms/macOS/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/platforms/macOS/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/platforms/macOS/.zprofile ~/.zprofile
@@ -19,12 +21,19 @@ ln -sf ~/dotfiles/platforms/macOS/.latexmkrc ~/.latexmkrc
 ln -sf ~/dotfiles/platforms/macOS/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/platforms/macOS/.vrapperrc ~/.vrapperrc
 ln -sf ~/dotfiles/platforms/macOS/.xvimrc ~/.xvimrc
+
 if ! xcode-select -p >/dev/null 2>&1; then
   xcode-select --install
 fi
+
 if ! command -v brew >/dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+brew bundle --file=~/dotfiles/platforms/macOS/Brewfile
+
+sh ~/dotfiles/common/synbolic_link.sh
+sh ~/dotfiles/common/nvim/setup_nvim.sh
+
 if [ ! -e ~/.config/ghostty ]; then
   mkdir -p ~/.config/ghostty
 fi
