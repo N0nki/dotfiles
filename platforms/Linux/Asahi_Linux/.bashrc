497e763f16b550fec184a4756f297fdebd4e23a3
N0nki <rgstg_land@icloud.com>
2026-01-11 14:49:00 +0900

    add Fedra Asahi Linux

---
+++ platforms/Linux/Asahi_Linux/.bashrc
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+  . /etc/bashrc
+fi
+
+# User specific environment
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
+  PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+  for rc in ~/.bashrc.d/*; do
+    if [ -f "$rc" ]; then
+      . "$rc"
+    fi
+  done
+fi
+unset rc
+
+# my config
+
+# history
+export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S %a "
+export HISTSIZE=10000
+export HISTFILESIZE=10000
+HISTCONTROL=ignoreboth
+shopt -s histappend
+
+# startship
+eval "$(starship init bash)"
+
+# direnv
+eval "$(direnv hook bash)"
+
+# fzf
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+# fzf-git: Git branch/commit/tag selection with fzf
+[ -f ~/dotfiles/common/fzf-git/fzf-git.sh ] && source ~/dotfiles/common/fzf-git/fzf-git.sh
---
+++ platforms/Linux/Asahi_Linux/setup.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Fedora Asahi Linux Setup Script
+
+set -eu
+
+sudo dnf upgrade -y
+
+sudo dnf install -y \
+  snap \
+  vim \
+  gcc \
+  ripgrep \
+  go \
+  shellcheck \
+  shfmt \
+  direnv
+
+# Install Neovim via snap
+#
+echo ""
+echo "==> Installing Neovim..."
+sudo snap install nvim --classic
+
+# Install yq via snap
+#
+echo ""
+echo "==> Installing yq..."
+sudo snap install yq
+
+#
+# Install yamlfmt via snap
+#
+echo ""
+echo "==> Installing yamlfmt..."
+sudo snap install yamlfmt
+
+#
+# Install Starship prompt
+#
+echo ""
+echo "==> Installing Starship..."
+curl -sS https://starship.rs/install.sh | sh -s -- -y
+
+# Install Node.js via n
+#
+echo ""
+echo "==> Installing Node.js..."
+sudo dnf install -y npm
+sudo npm i -g n
+sudo n stable
+sudo dnf remove -y npm
+
+# Install prettier (markdown formatter)
+#
+echo ""
+echo "==> Installing prettier..."
+sudo npm i -g prettier
+
+#
+# Install tree-sitter-cli
+#
+echo ""
+echo "==> Installing tree-sitter-cli..."
+sudo npm i -g tree-sitter-cli
+
+# Install fzf (fuzzy finder)
+#
+echo ""
+echo "==> Installing fzf..."
+if [ ! -d ~/.fzf ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --all --no-update-rc
+else
+  echo "fzf already installed"
+fi
+
+# Run common/synbolic_link.sh
+#
+echo ""
+echo "==> Running common/synbolic_link.sh..."
+sh ~/dotfiles/common/synbolic_link.sh || true
+
+#
+# Setup Neovim configuration
+#
+echo ""
+echo "==> Setting up Neovim..."
+sh ~/dotfiles/common/nvim/setup_nvim.sh
+
+#
+# Link platform-unified .bashrc
+#
+echo ""
+echo "==> Linking .bashrc..."
+ln -sf ~/dotfiles/platforms/Linux/Asahi_Linux/.bashrc ~/.bashrc
