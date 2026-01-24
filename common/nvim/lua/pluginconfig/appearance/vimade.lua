22fd3d31f015e22c9ddd59f9638572c7503102a7
N0nki <rgstr_land@icloud.com>
2026-01-25 02:10:38 +0900

    update

--- common/nvim/lua/pluginconfig/appearance/vimade.lua
+++ common/nvim/lua/pluginconfig/appearance/vimade.lua
@@ -2,7 +2,7 @@ require("vimade").setup({
     recipe = { "default", { animate = true } },
     -- fadelevel: 非アクティブウィンドウの暗さ (0.0-1.0)
     -- 0.0 = 完全にフェード, 1.0 = フェードなし
-    fadelevel = 0.8,
+    fadelevel = 0.7,
     -- ncmode: フェード対象の選択
     -- 'buffers': 同じバッファを表示していないウィンドウをフェード
     -- 'windows': 全ての非アクティブウィンドウをフェード
