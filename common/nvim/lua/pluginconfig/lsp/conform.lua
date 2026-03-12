8a99b1ee5b6ac1f218baac53bd9f944217b7013b
N0nki <rgstr_land@icloud.com>
2026-03-12 15:49:28 +0900

    add format enable/disable commands

--- common/nvim/lua/pluginconfig/lsp/conform.lua
+++ common/nvim/lua/pluginconfig/lsp/conform.lua
@@ -21,6 +21,9 @@ require("conform").setup({
         },
     },
     format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+        end
         local bufname = vim.api.nvim_buf_get_name(bufnr)
         local ignore_patterns = { "/fzf%-git/" }
         for _, pattern in ipairs(ignore_patterns) do
@@ -34,3 +37,16 @@ require("conform").setup({
         }
     end,
 })
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+        vim.b.disable_autoformat = true
+    else
+        vim.g.disable_autoformat = true
+    end
+end, { desc = "Disable autoformat-on-save", bang = true })
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+end, { desc = "Enable autoformat-on-save" })
