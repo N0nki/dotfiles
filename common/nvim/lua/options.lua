3cc0502d9196d03075f295dbdf480ae4e1fabff9
N0nki <rgstr_land@icloud.com>
2026-04-26 04:54:17 +0900

    disable unused remote plugin providers

--- common/nvim/lua/options.lua
+++ common/nvim/lua/options.lua
@@ -74,6 +74,11 @@ vim.o.splitbelow = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 vim.opt.spell = true
 vim.opt.spelllang = { "en_us" }
 
